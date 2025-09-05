c26b28a68cc848e56c5288d75907af5898ea1c68
Zander Vilaysane <zandervilaysane@gmail.com>
2025-09-04 18:54:31 -0700

    vercel deployment build resolved

---
+++ friends.js
@@ -0,0 +1,102 @@
+export const FRIENDS = {
+  "ryanPW": {
+    name: "Ryan",
+    message: "hey man, i've so so fortunate to had gotten close to you after COGS 107A (f dat class). You brought an absurd amount of growth and laughter into my life. I'm grateful in how you taught me by example of how to be a considerate, spontaneous friend who's (for the most part) always down for a plot. You have a heart so massive it's hung asf. I admire you for how you love and how you go about enjoying life. I'm eternally grateful for you and I'm excited to see where our friendship goes. I'm also thankful for you handling me being complain, bitch, and all of the chaos in between. Love you budbud :)",
+    image: "/images/ryan.jpg"
+  },
+  "natPW": {
+    name: "Nat",
+    message: "gnat nat. oho how grateful i am to have met you and experienced college with you. you taught me how to live, how to grind, and say fuck it we ball in every aspect. you showed me a true definition of work hard and play hard. i always admire how you do it all, even if it means a week of heavy rotting. you paved a path of enjoying things i never imagined i would enjoy. you always encourage me to strive to be a better version of myself and for that im forever grateful. i luv my lesbian sm and im so excited to see what adventures await for us in adulthood. love u crazy fool.",
+    image: "/images/nat.jpg"
+  },
+  "adrielPW": {
+    name: "Adriel",
+    message: "hey sha boi boi. wow. im fortunate to see how much you've grown as a person and how you influenced my scope in life. you have shown me by example on how to partake in my creative endeavors and remind me why creativity is so beautiful and rewarding. i enjoy seeing how you enjoy things. im grateful i was allowed to see you vulnerable and see you resolve your conflicts. i admire how disciplined you are and i appreciate how oftentimes i don't need to overthink things and rather just be straight up. sorry again i was a lil too blunt abt some things, seeing it now i would have approached it differently but regardless im appreciative you were receptive of our feedback and you grew positively. always rooting you in the endeavors you put yourself to, and im excited to see what your creative brain continues on creating. hope to see you more and enjoy life further in this friendship. manifesting the functions and opportunities in which we both can interact hehe. we both finna make it out greatly hehehehehehe",
+    image: "/images/adriel.jpg"
+  },
+  "kadiePW": {
+    name: "Kadie",
+    message: "filler",
+    image: "/images/kadie.jpg"
+  },
+  "snehaPW": {
+    name: "Sneha",
+    message: "filler",
+    image: "/images/sneha.jpg"
+  },
+  "rileyPW": {
+    name: "Riley",
+    message: "filler",
+    image: "/images/Riley.jpg"
+  },
+  "sahilPW": {
+    name: "Sahil",
+    message: "filler",
+    image: "/images/sahil.jpg"
+  },
+  "natePW": {
+    name: "Nate",
+    message: "filler",
+    image: "/images/nate.jpg"
+  },
+  "jarenzPW": {
+    name: "Jarenz",
+    message: "filler",
+    image: "/images/jarenz.jpg"
+  },
+  "hannahPW": {
+    name: "Hannah",
+    message: "filler",
+    image: "/images/hannah.jpg"
+  },
+  "donovanPW": {
+    name: "Donovan",
+    message: "filler",
+    image: "/images/donovan.jpg"
+  },
+  "djPW": {
+    name: "DJ",
+    message: "filler",
+    image: "/images/dj.jpg"
+  },
+  "estherPW": {
+    name: "Esther",
+    message: "filler",
+    image: "/images/esther.jpg"
+  },
+  "jonathanPW": {
+    name: "Jonathan",
+    message: "filler",
+    image: "/images/jonathan.jpg"
+  },
+  "derrickPW": {
+    name: "Derrick",
+    message: "filler",
+    image: "/images/derrick.jpg"
+  },
+  "ednaPW": {
+    name: "Edna",
+    message: "filler",
+    image: "/images/edna.jpg"
+  },
+  "zairaPW": {
+    name: "Zaira",
+    message: "filler",
+    image: "/images/zaira.jpg"
+  },
+  "jacobPW": {
+    name: "Jacob",
+    message: "filler",
+    image: "/images/jacob.jpg"
+  },
+  "mateoPW": {
+    name: "Mateo",
+    message: "filler",
+    image: "/images/mateo.jpg"
+  },
+  "aprilPW": {
+    name: "April",
+    message: "filler",
+    image: "/images/april.jpg"
+  },
+};
